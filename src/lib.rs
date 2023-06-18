e473e8cd759efec2754e17fc2b4a0e131eecbfc1
rbits0 <rbits0@gmail.com>
2023-06-18 17:34:20 +1000

    Implement parse_string()

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
-const VALID_CHARS = ['a', 'b',];
+#[derive(Debug, PartialEq)]
+enum Instruction {
+    Increment,
+    Decrement,
+    Left,
+    Right,
+    Open,
+    Close,
+    Input,
+    Output,
+}
 
 
-fn remove_comments(code: &str) -> String {
-    code.chars().filter(|char| ).collect()
+fn parse_string(code: &str) -> Vec<Instruction> {
+    code.chars().filter_map({
+        |x| match x {
+            '+' => Some(Instruction::Increment),
+            '-' => Some(Instruction::Decrement),
+            '<' => Some(Instruction::Left),
+            '>' => Some(Instruction::Right),
+            '[' => Some(Instruction::Open),
+            ']' => Some(Instruction::Close),
+            ',' => Some(Instruction::Input),
+            '.' => Some(Instruction::Output),
+            _ => None,
+        }
+    }).collect()
 }
 
 fn run(code: &str) {
@@ -15,7 +37,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn remove_comments_test() {
-        assert_eq!("<<<", remove_comments("a<b<c<d"))
+    fn parse_string_test() {
+        assert_eq!(
+            vec![Instruction::Left, Instruction::Increment, Instruction::Left, Instruction::Left, Instruction::Close],
+            parse_string("a<+<c<]")
+        );
     }
 }
