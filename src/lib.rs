5acb89efec9b43f4ba002ee1e23e7a81d73ceda8
rbits0 <rbits0@gmail.com>
2023-06-20 13:34:09 +1000

    Maybe implemented it? Not tested

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, PartialEq)]
+use std::{error::Error, io::{self, Read}};
+
+#[derive(Debug, PartialEq, Eq)]
 pub enum Instruction {
     Increment,
     Decrement,
@@ -27,8 +29,106 @@ pub fn parse_string(code: &str) -> Vec<Instruction> {
     }).collect()
 }
 
-pub fn run(code: &str) {
+pub fn run(code: &str) -> Result<(), Box<dyn Error>>{
     let instructions = parse_string(code);
+    // Location of the instruction pointer
+    let mut i: usize = 0;
+    // Location of the data pointer
+    let mut pointer: usize = 0;
+    // Using u8, but max will be 127
+    let mut data: Vec<u8> = vec![0];
+    
+    while i < instructions.len() {
+        match instructions[i] {
+            Instruction::Increment => {
+                if data[pointer] == 127 {
+                    data[pointer] = 0;
+                } else {
+                    data[pointer] += 1;
+                }
+            },
+            Instruction::Decrement => {
+                if data[pointer] == 0 {
+                    data[pointer] = 127;
+                } else {
+                    data[pointer] -= 1;
+                }
+            },
+            Instruction::Left => {
+                pointer.saturating_sub(1);
+            },
+            Instruction::Right => {
+                pointer += 1;
+                if pointer >= data.len() {
+                    data.push(0);
+                }
+            },
+            Instruction::Open => {
+                let remaining_instructions = &instructions[(pointer + 1)..];
+                pointer = find_matching_bracket(remaining_instructions, true)? + pointer + 1;
+            },
+            Instruction::Close => {
+                let remaining_instructions = &instructions[..pointer];
+                pointer = find_matching_bracket(remaining_instructions, false)?;
+            },
+            Instruction::Input => {
+                let mut input: [u8; 1] = [0];
+                io::stdin().read(&mut input).map_err(|_| "failed to read input")?;
+                if input[0] > 127 {
+                    return Err("input must be an ASCII character".into());
+                }
+                data[pointer] = input[0];
+            },
+            Instruction::Output => {
+                let output = data[pointer] as char;
+                print!("{}", output);
+            }
+        }
+
+        panic!("reached end of loop");
+    }
+    
+    Ok(())
+}
+
+
+pub fn find_matching_bracket(instructions: &[Instruction], forwards: bool) -> Result<usize, &'static str> {
+    let mut level = 0;
+    if forwards {
+        for (i, instruction) in instructions.iter().enumerate() {
+            match instruction {
+                Instruction::Open => {
+                    level += 1;
+                },
+                Instruction::Close => {
+                    if level > 0 {
+                        level -= 1
+                    } else {
+                        return Ok(i);
+                    }
+                },
+                _ => (),
+            }
+        }
+    } else {
+        for (i, instruction) in instructions.iter().enumerate().rev() {
+            match instruction {
+                Instruction::Open => {
+                    if level > 0 {
+                        level -= 1
+                    } else {
+                        return Ok(i);
+                    }
+                },
+                Instruction::Close => {
+                    level += 1;
+                },
+                _ => (),
+            }
+        }
+    }
+    
+    Err("all brackets must have matching brackets")
 }
 
 
@@ -39,8 +139,24 @@ mod tests {
     #[test]
     fn parse_string_test() {
         assert_eq!(
-            vec![Instruction::Left, Instruction::Increment, Instruction::Left, Instruction::Left, Instruction::Close],
-            parse_string("a<+<c<]")
+            parse_string("a<+<c<]"),
+            vec![Instruction::Left, Instruction::Increment, Instruction::Left, Instruction::Left, Instruction::Close]
+        );
+    }
+    
+    #[test]
+    fn forward_match() {
+        assert_eq!(
+            Ok(5),
+            find_matching_bracket(&parse_string("-[-]-]]--"), true)
+        );
+    }
+    
+    #[test]
+    fn backward_match() {
+        assert_eq!(
+            Ok(3),
+            find_matching_bracket(&parse_string("--[[-[-]-"), false)
         );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ use std::{env, fs, error::Error};
 use bf_rs::*;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let Some(filepath) = env::args().nth(1) else {
-        return Err("A");
-    };
-    let code_string = fs::read_to_string(filepath);
+    let filepath = env::args().nth(1).ok_or("missing file path argument")?;
+    let code_string = fs::read_to_string(filepath)?;
 
     run(&code_string);
     
