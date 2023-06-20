44d8c83e0e4cb57fc735799d9ba68d856553dd42
rbits0 <rbits0@gmail.com>
2023-06-20 17:40:14 +1000

    Propagate errors from run()

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let filepath = env::args().nth(1).ok_or("missing file path argument")?;
     let code_string = fs::read_to_string(filepath)?;
 
-    run(&code_string);
+    run(&code_string)?;
     
     Ok(())
 }
