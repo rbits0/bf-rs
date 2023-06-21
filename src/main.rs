c86a9b1ff5ccd1c8e83f185a180af7d740c0c9cd
rbits0 <rbits0@gmail.com>
2023-06-21 16:14:21 +1000

    Add argument parser

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,44 @@
-use std::{env, fs, error::Error};
+use std::{fs, error::Error, env};
 use bf_rs::*;
+use clap::{Parser, ValueEnum};
+
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    filepath: String,
+
+    #[arg(short, long, value_enum, default_value_t=DebugMode::None)]
+    debug_mode: DebugMode,
+    
+    /// Enable breakpoints
+    #[arg(short='b', long)]
+    breakpoints: bool,
+
+    /// Enable macros
+    #[arg(short='m', long)]
+    macros: bool,
+}
+
+
+#[derive(ValueEnum, Clone, Debug, PartialEq)]
+enum DebugMode {
+    None,
+    
+    /// Print memory and instructions
+    Verbose,
+    
+    /// Verbose, but pause at every instruction
+    Step,
+}
+
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let filepath = env::args().nth(1).ok_or("missing file path argument")?;
+    let args = Args::parse();
+    
+    let filepath = args.filepath;
     let code_string = fs::read_to_string(filepath)?;
-
+    
     run(&code_string)?;
     
     Ok(())
